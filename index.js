10c0604f0d626dc8cdb75a0510416d94af138ab9
Matthew O'Brien <matthew.kevin.obrien@gmail.com>
2021-06-09 16:36:29 -0400

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(match => 
+        match.toLowerCase() === name.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, testString) {
+    return drivers.filter( match =>
+      match.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter( record => record.name === name)
+  }
